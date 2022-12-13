3c18cb9f08ad3674bab365ecef8915d4eec94fc8
Sebastine Ogu (Thrive) <sebastinoptimum@gmail.com>
2022-12-13 15:48:40 +0100

    updated main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { ApolloServer } from 'apollo-server';
-
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
+
+
+const PORT = process.env.PORT || 4001;
+
 const server = new ApolloServer({
+
     typeDefs: typeDefs,
     resolvers: resolvers
 });
 
-server.listen().then(({ url }) => {
+server.listen({
+    port: PORT
+}).then(({ url }) => {
+
     console.log(`🚀  Server ready at ${url}`);
 });
